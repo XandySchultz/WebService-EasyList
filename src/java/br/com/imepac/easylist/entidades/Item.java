3ca3dee1d5f6bfeb4ab2bf8bd1aba991e0d29403
unknown <171-001051@IMEPACAD.local>
2019-04-05 20:47:10 -0300

    Primeiro commit

---
+++ src/java/br/com/imepac/easylist/entidades/Item.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.imepac.easylist.entidades;
+
+/**
+ *
+ * @author 171-001051
+ */
+public class Item extends Lista{
+    private int id;    
+    private String nome;
+    private float preco;
+    private String descricao;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the preco
+     */
+    public float getPreco() {
+        return preco;
+    }
+
+    /**
+     * @param preco the preco to set
+     */
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+    
+}
---
+++ src/java/br/com/imepac/easylist/recursos/ListaWS.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.imepac.easylist.recursos;
+
+import javax.ws.rs.Path;
+
+/**
+ *
+ * @author 171-001051
+ */
+@Path("listas")
+public class ListaWS {
+    
+}
